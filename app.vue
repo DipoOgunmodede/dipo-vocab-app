d39ef1439b9220c16489022b659e97e0b84a0e95
Dipo Ogunmodede <dipoogunmodede@gmail.com>
2024-05-21 17:38:13 +0100

    fixed broken word checks

--- app.vue
+++ app.vue
@@ -1,12 +1,11 @@
 <template>
-  {{gameState}}
+  {{ gameState }}
   <UContainer>
     <UCard class="mt-10">
       <h1>Enter as many {{ currentWordType }} as you can in {{ numberOfSeconds }} seconds</h1>
-      <UInput v-model="userInputWord" @keyup.enter="checkWord" @keyup.space="checkWord" />
-      <UButton @click="checkWord" icon="i-heroicons-book-open" target="_blank">Check word
+      <UInput v-model="userInputWord" @keyup.enter="checkCurrentWord" @keyup.space="checkCurrentWord" />
+      <UButton @click="checkCurrentWord" icon="i-heroicons-book-open" target="_blank">Check word
       </UButton>
-      <p v-if="checkWordCompleted && userInputWord">{{ validationMessage }}</p>
       <Timer :seconds="numberOfSeconds" @gameStateChanged="updateGameState" />
     </UCard>
     <!-- <ScoreCounter @increment="incrementScore" /> -->
@@ -18,43 +17,60 @@ import nlp from 'compromise';
 let gameState = ref('inactive');
 let userInputWord = ref('');
 let numberOfSeconds = 100;
-let wordTypes = ['verbs' , 'nouns', 'adjectives', 'adverbs'];
-let currentWordType = computed(() => wordTypes[Math.floor(Math.random() * wordTypes.length)]);
-let isWordValid = ref(false);
-let checkWordCompleted = ref(false);
+let currentWordType = ref('');
+let currentScore = ref(0);
+let lastInputWordValid = ref(false);
 
-watch(userInputWord, () => {
-  checkWordCompleted.value = false;
-});
+// watch(userInputWord, () => {
+//   lastInputWordValid.value = false;
+// });
 
 let validationMessage = computed(() => {
-  return isWordValid.value ? 'The word is valid.' : 'The word is not valid.';
+  return lastInputWordValid.value ? 'The word is valid.' : 'The word is not valid.';
 });
 
 function updateGameState(newState) {
   gameState.value = newState;
 }
 
+const wordTypeFunctions = {
+  verbs: () => nlp(userInputWord.value).verbs(),
+  nouns: () => nlp(userInputWord.value).nouns(),
+  adjectives: () => nlp(userInputWord.value).adjectives(),
+  adverbs: () => nlp(userInputWord.value).adverbs(),
+};
 
-function checkWord() {
-  const wordTypeFunctions = {
-    verbs: () => nlp(userInputWord.value).verbs(),
-    nouns: () => nlp(userInputWord.value).nouns(),
-    adjectives: () => nlp(userInputWord.value).nouns().adjectives(),
-    adverbs: () => nlp(userInputWord.value).verbs().adverbs(),
-  };
-
-  const checkFunction = wordTypeFunctions[currentWordType.value];
-  isWordValid.value = checkFunction && checkFunction().length > 0;
-
-  // If the word is valid, randomize the word type for the next word
-  if (isWordValid.value) {
-    currentWordType.value = wordTypes[Math.floor(Math.random() * wordTypes.length)];
-    userInputWord.value = ''; // Clear the input field
+function checkCurrentWord() {
+  //this checks the currently input with with the corresponding method 
+  const selectWordCheckFunction = wordTypeFunctions[currentWordType.value];
+  console.log('checking word');
+  console.log(selectWordCheckFunction())
+  lastInputWordValid.value = selectWordCheckFunction && selectWordCheckFunction().length > 0;
+  //if the word is valid increment score, select a new word type randomly, and set lastwordvalid to true and also checkWordCompleted to true
+  console.log(lastInputWordValid.value)
+  if (lastInputWordValid.value) {
+    incrementScore();
+    selectNewWordType();
+    lastInputWordValid.value = true;
+    userInputWord.value = '';
   }
-
-  checkWordCompleted.value = true;
 }
+
+const selectNewWordType = () => {
+  const wordTypes = ['adjectives' ];
+  currentWordType.value = wordTypes[Math.floor(Math.random() * wordTypes.length)];
+};
+
+
+
+
+function incrementScore() {
+  console.log('incrementing score');
+  currentScore.value += 1;
+}
+
+onMounted(selectNewWordType);
+
 </script>
 
 <style scoped>
